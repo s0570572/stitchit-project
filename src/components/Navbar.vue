744bc521351b3d0f4ad93e1489daf51433d70a40
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-11-20 15:37:30 +0100

    add link active class

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,8 +7,8 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-          <router-link class="nav-link active" to="/">Home</router-link>
-          <router-link class="nav-link active" to="/entries">Gallery</router-link>
+          <router-link class="nav-link" to="/">Home</router-link>
+          <router-link class="nav-link" to="/entries">Gallery</router-link>
           <router-link class="nav-link" to="/about">About</router-link>
         </div>
       </div>

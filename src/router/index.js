82ecc0b16bd463af8a4d92b988077240197fabf0
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-11-20 15:26:24 +0100

    Hello World demo

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Gallery from '../views/Gallery.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/entries',
+    name: 'Gallery',
+    component: Gallery
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/earth-blue-planet-globe-planet-87651.jpeg">
+    <HelloWorld msg="Hello World!"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>

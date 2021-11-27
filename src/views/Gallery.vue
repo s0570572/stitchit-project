933034749a9b41d8bf168817d5d5b6e715051ed3
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-11-27 20:12:31 +0100

    add an accordeon component for future entries

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -1,13 +1,15 @@
 <template>
   <topic-tab></topic-tab>
+  <accordeon></accordeon> // eventually move to a different component for the gallery view body with a router and add a padding
 </template>
 
 <script>
 import TopicTab from '../components/TopicTab'
+import Accordeon from '../components/Accordeon'
 
 export default {
   name: 'Gallery',
-  components: { TopicTab }
+  components: { TopicTab, Accordeon }
 }
 </script>
 

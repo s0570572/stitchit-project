31c2348dded891cc3e050aa904fc7b6b44e4f0bc
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-11-28 09:57:56 +0100

    delete old image

--- src/components/Picturebar.vue
+++ src/components/Picturebar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid">
-    <img alt="Mouline threads" class="img-fluid" src="../assets/threads.jpg">
+    <img alt="Mouline threads" class="img-fluid" src="../assets/background.jpg">
   </div>
 </template>
 

b260495095820e8cbc4ccd5a3670f50d0715d4a9
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-12-14 21:53:10 +0100

    complete the About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,22 @@
 <template>
   <div id="intro">
-    <h1>This is an about page</h1>
+    <h3><strong>About StitchIt!</strong></h3>
+    <figure id="text">
+      <p>The StitchIt! project is a non-profit project for sharing free stitching schemes. Schemes shared on this
+        platform are not subject for sale.</p>
+      <p>Please obstain from posting links to irrelevant materials.</p>
+      <p>The StitchIt! project team wishes you a nice stay on this platform and tons of inspiration!</p>
+    </figure>
   </div>
 </template>
 
 <style>
 #intro {
   background-color: #a1ba8b;
-  padding: 18%;
+  padding: 14%;
+}
+#text {
+  text-align: left;
+  padding: 5px;
 }
 </style>

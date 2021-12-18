a46a6064654ad79d1eefe7fe822f1d4901d1eb64
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-12-18 21:40:05 +0100

    adapt unit test to the new project stucture

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="textbox">
     <h3 id="headingpadding"><strong>Hello and welcome to the StitchIt! project!</strong></h3>
-    <p class="text"><em>This is where you can find a cross-stitch scheme of your liking for free. Just don't hesitate to go through all the different topic tabs to look for your favourite motives and choose the difficulty level that you want to tackle.</em></p>
+    <p class="text"><em>{{ msg }}This is where you can find a cross-stitch scheme of your liking for free. Just don't hesitate to go through all the different topic tabs to look for your favourite motives and choose the difficulty level that you want to tackle.</em></p>
     <p class="text"><em>Do you create schemes of your own and want to share them? Feel free to do so by clicking on the dark green 'add' button in the upper right corner. You will be then guided through a simple uploading procedure.</em></p>
     <p class="text"><em><strong>Now take your time and enjoy the StitchIt! project!</strong></em></p>
   </div>
@@ -11,7 +11,8 @@
 
 export default {
   name: 'Home',
-  components: {
+  props: {
+    msg: String
   }
 }
 </script>

dc798a8b77bcc53879886a3130da60e2856841a2
ElenaAkhmerova <Elenaakhmerovamayflower@outlook.de>
2021-12-19 12:39:24 +0100

    move listalignment into accordeon

--- src/components/Accordeon.vue
+++ src/components/Accordeon.vue
@@ -24,7 +24,24 @@
           </button>
         </h2>
         <div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
-          <div class="accordion-body"><listalignment></listalignment></div>
+          <div class="accordion-body">
+            <dl class="row">
+              <dt class="col-sm-3">Title:</dt>
+              <dd class="col-sm-8">Placeholder for the title.</dd>
+
+              <dt class="col-sm-3">Description:</dt>
+              <dd class="col-sm-9">Placeholder for the description.</dd>
+
+              <dt class="col-sm-3">Topic:</dt>
+              <dd class="col-sm-9">Placeholder for the topic.</dd>
+
+              <dt class="col-sm-3">Difficulty level:</dt>
+              <dd class="col-sm-9">Placeholder for the difficulty level.</dd>
+
+              <dt class="col-sm-3">Link:</dt>
+              <dd class="col-sm-9">Placeholder for the link.</dd>
+            </dl>
+          </div>
         </div>
       </div>
     </div>
@@ -33,13 +50,10 @@
 </template>
 
 <script>
-import Listalignment from './Listalignment'
 
 export default {
   name: 'Accordeon',
-  components: {
-    Listalignment
-  }
+  components: { }
 }
 </script>
 
@@ -50,5 +64,15 @@ export default {
 }
 #abstand {
   padding-bottom: 30px;
+}
+.row {
+  --bs-gutter-x: 1.5rem;
+  --bs-gutter-y: 0;
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: calc(-1 * var(--bs-gutter-y));
+  margin-right: calc(-.5 * var(--bs-gutter-x));
+  margin-left: calc(-.5 * var(--bs-gutter-x));
+  text-align: left;
 }
 </style>
